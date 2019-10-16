c5f40612cc3e0d6f8fdb5180a2f2d337f3770be0
maclo4 <fy1434@wayne.edu>
2019-10-16 12:43:51 -0400

    added more to the initialize function

--- new database project/src/com/luv2code/jsp/DbConnect.java
+++ new database project/src/com/luv2code/jsp/DbConnect.java
@@ -55,10 +55,15 @@ public class DbConnect{
 			preparedStatement.setString(2, price);
 			preparedStatement.setString(3, description);
 			
+			boolean rowInserted = false;
 			
-//			preparedStatement.executeUpdate();
-			 boolean rowInserted = preparedStatement.executeUpdate() > 0;
-		        preparedStatement.close();
+			// try blocks so that the system doesn't crash when sql statements are rejected
+			try {
+			 rowInserted = preparedStatement.executeUpdate() > 0;
+		        preparedStatement.close();}
+			catch(Exception e){
+				System.out.println(e);}
+			
 //		        disconnect();
 		        return rowInserted;
 		    }     
@@ -66,23 +71,58 @@ public class DbConnect{
 	  public boolean insertCategory(String item, String category) throws SQLException {
 		  String sql0 = "INSERT INTO ItemCategories(ItemID, Category) VALUES (" + 
 		  		"	(SELECT ID FROM Items WHERE title = ?)," + 
-		  		"     ?)";
+		  		"     ?)";	
+		  Boolean rowInserted = false;
 		  preparedStatement = (PreparedStatement) connect.prepareStatement(sql0);
 			preparedStatement.setString(1, item);
 			preparedStatement.setString(2, category);
-			
-			boolean rowInserted = preparedStatement.executeUpdate() > 0;
-	        preparedStatement.close();
+		
+			// try blocks so that the system doesn't crash when sql statements are rejected
+			try {
+			rowInserted = preparedStatement.executeUpdate() > 0;
+	        preparedStatement.close();}
+			catch(Exception e) {
+				System.out.println(e);}
 //	        disconnect();
 	        return rowInserted;
 	  }
 	  
-	  public boolean insertUsers(String userID, String password, String email,
-			  String firstName, String lastName, String gender, String age) throws SQLException {
+	  public boolean insertUser(String userID, String password, String email,
+			  String firstName, String lastName, String gender, String age, Boolean admin) throws SQLException {
 		  
-		  
-		  return true;
+		  //store sql statement as string
+		  String sql0 = "INSERT INTO Users(UserID, Password, Email, FirstName, LastName, Gender, Age, Admin)\r\n" + 
+		  		" VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		 
+		  String adminString;
+		  Boolean rowInserted = false;
+		  if(admin == true)
+			  adminString = "1";
+		  else
+			  adminString = "0";
+		  // create and load prepared statement
+		  	preparedStatement = (PreparedStatement) connect.prepareStatement(sql0);
+		  	preparedStatement.setString(1, userID);
+			preparedStatement.setString(2, password);
+			preparedStatement.setString(3,email);
+			preparedStatement.setString(4, firstName);
+			preparedStatement.setString(5, lastName);
+			preparedStatement.setString(6, gender);
+			preparedStatement.setString(7, age);
+			preparedStatement.setString(8, adminString);
+			
+			try {
+			//execute prepared statement
+			rowInserted = preparedStatement.executeUpdate() > 0;
+		        preparedStatement.close();}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+
+		        return rowInserted;
 	  }
+	  
+	  // big function that just initializes all the necessary tables using other insert functions
 	  public boolean initializeDb() throws SQLException{
 		  
 		 	connect_func();  
@@ -90,7 +130,7 @@ public class DbConnect{
 		 	
 		 	String sql0 = "DROP TABLE IF EXISTS ItemCategories";
 		 	String sql1 = "drop table IF EXISTS Items" ;
-		 	//String sql12 = "DROP TABLE"
+		 	String sql12 = "DROP TABLE IF EXISTS Users";
 		 		
 		 	String sql2 =	"CREATE TABLE Items(" + 
 		 			"Title varchar(50)," + 
@@ -115,17 +155,23 @@ public class DbConnect{
 		 			"LastName varchar(40) NOT NULL," + 
 		 			"Gender varchar(10) NOT NULL," + 
 		 			"Age int(3) NOT NULL," + 
+		 			"Admin boolean NOT NULL," + 
 		 			"PRIMARY KEY(UserID)," + 
 		 			"CHECK (Gender='Male' OR Gender = \"Female\" OR Gender = \"Non-binary\")," + 
 		 			"CHECK (Email like '%_@__%.__%'));";
 		 	
+		 	// drop the tables then recreate them
 		 	statement =  (Statement) connect.createStatement();
 		 	statement.executeUpdate(sql0);
 		 	statement.executeUpdate(sql1);
+		 	statement.executeUpdate(sql12);
 		 	statement.executeUpdate(sql2);
 		 	statement.executeUpdate(sql3);
+		 	statement.executeUpdate(sql4);
+		 	
 		 	System.out.println("tables created.");
 		 	
+		 	// initialize items table
 		 	DbConnect test = new DbConnect();
 		 	test.insertItem("Banana", "12.50", "Yellow and Yummy");
 		 	test.insertItem("Dvd", "12.00", "Fast and Furious");
@@ -138,7 +184,7 @@ public class DbConnect{
 		 	test.insertItem("iPhone X", "1200.00", "Black");
 		 	test.insertItem("China Plates", "120.50", "Genuine ceramics");
 		 	
-		 	
+		 	// initialize categories for items
 		 	test.insertCategory("Banana", "fruit");
 		 	test.insertCategory("Banana", "food");
 		 	test.insertCategory("Dvd", "entertainment");
@@ -151,7 +197,8 @@ public class DbConnect{
 		 	test.insertCategory("iPhone", "technology");
 		 	test.insertCategory("China Plates", "home");
 		 	
-		 	
+		 	// initialize user table
+		 	test.insertUser("root", "pass1234", "scott@gmail.com", "Scott", "Howard", "Male", "22", true);
 	        return true;
 			/*String sql = "insert into Items(title, price) values (?, ?)";
 			preparedStatement = (PreparedStatement) connect.prepareStatement(sql);
